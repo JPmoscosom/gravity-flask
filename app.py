311addd12e03fed25306ecff7d56705750b66f0e
Juan Moscoso <juanmos.dev@gmail.com>
2024-04-01 21:47:58 -0500

    to deploy, Prueba correcion en blueprints

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ def page_not_found(e):
 def hello_world():
     return "Esta es la API de avioncitos"
 
-
+app.register_blueprint(Aviones.main, url_prefix='/api/aviones')
 # Set-Ups
 if __name__ == '__main__':
     # app.config.from_object(configuration['development'])
     # Blueprints
-    app.register_blueprint(Aviones.main, url_prefix='/api/aviones')
+
     app.register_blueprint(Vuelos.main, url_prefix='/api/vuelos')
     app.register_error_handler(404, page_not_found)
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 443)), use_reloader=False)
