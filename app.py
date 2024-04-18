a5c43ec7e875d88fec8115a8e8cc0e3571158ee7
Juan Moscoso <juanmos.dev@gmail.com>
2024-04-18 17:06:42 -0500

    to deploy, Cambios en el POST

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask, jsonify
-from routes import Aviones, Vuelos
 import os
+
+from flask import Flask
+from flask import jsonify, request
+
+from models.AvionModel import AvionModel
+from models.entities.Avion import Avion
+from routes import Aviones, Vuelos
 
 app = Flask(__name__)
 
 
 def page_not_found(e):
     return "<h1>404 Page not found</h1>", 404
+
+
+@app.route('/add', methods=['POST'])
+def add_avion():
+    try:
+        if request.method == "POST":
+            matricula = request.json['matricula']
+            fabricante = request.json['fabricante']
+            modelo = request.json['modelo']
+            fecha_fabricacion = request.json['fecha_fabricacion']
+            capacidad_pasajeros = request.json['capacidad_pasajeros']
+            rango = request.json['rango']
+            estado = request.json['estado']
+            propietario = request.json['propietario']
+            avion = Avion(matricula, fabricante, modelo, fecha_fabricacion, capacidad_pasajeros
+                          , rango, estado, propietario)
+            print(avion.matricula)
+            affected_rows = AvionModel.add_avion(avion)
+            if affected_rows == 1:
+                return jsonify({'message': 'Ok'}, 200)
+            else:
+                return jsonify({'message': 'Error'}, 500)
+        else:
+            return 'Esperando una entrada.....'
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route("/health", methods=["GET"])
